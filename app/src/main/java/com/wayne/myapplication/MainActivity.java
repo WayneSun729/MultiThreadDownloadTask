0ab4d327f46b69adac940ce77f4480055cf17641
Wayne <1127113613@qq.com>
2021-07-20 23:38:37 +0800

    设计了主页的文字提示跑马灯效果以及不能响应的button

--- app/src/main/java/com/wayne/myapplication/MainActivity.java
+++ app/src/main/java/com/wayne/myapplication/MainActivity.java
@@ -3,12 +3,16 @@ package com.wayne.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        message = findViewById(R.id.messageTextView);
+        message.setSelected(true);
     }
 }
