af4eee46a19ec6b3510fcfff12a29a99e32c5bc1
sunbowen <sunbowen@migu.cn>
2021-07-21 09:12:56 +0800

    新增了下载进度展示Activity，并为button添加了跳转功能

---
+++ app/src/main/java/com/wayne/myapplication/DownloadActivity.java
@@ -0,0 +1,17 @@
+package com.wayne.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author sunbowen
+ */
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
--- app/src/main/java/com/wayne/myapplication/MainActivity.java
+++ app/src/main/java/com/wayne/myapplication/MainActivity.java
@@ -2,17 +2,27 @@ package com.wayne.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * @author sunbowen
+ */
 public class MainActivity extends AppCompatActivity {
-    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        message = findViewById(R.id.messageTextView);
+        TextView message = findViewById(R.id.messageTextView);
+        Button downloadButton = findViewById(R.id.downloadButton);
         message.setSelected(true);
+        downloadButton.setOnClickListener(v -> {
+            Intent downloadActivityIntent = new Intent(this, DownloadActivity.class);
+            startActivity(downloadActivityIntent);
+        });
     }
 }
