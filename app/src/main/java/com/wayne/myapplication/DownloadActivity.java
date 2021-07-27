5c0a3941f7f82777d4d1d929d7a930539267bf21
Wayne <1127113613@qq.com>
2021-07-27 22:11:19 +0800

    加入了多线程下载文件

--- app/src/main/java/com/wayne/myapplication/DownloadActivity.java
+++ app/src/main/java/com/wayne/myapplication/DownloadActivity.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.text.DecimalFormat;
 import java.util.Objects;
 import java.util.concurrent.Executor;
@@ -42,8 +43,6 @@ public class DownloadActivity extends AppCompatActivity {
 
     private String responseData;
 
-    private String TAG = "Wayne";
-
     private final int UPDATE_TEXT = 1;
 
     private final int DOWNLOAD_SUCCESS = 2;
@@ -52,43 +51,28 @@ public class DownloadActivity extends AppCompatActivity {
 
     private final int DOWNLOAD_PROGRESS = 4;
 
+    private static final String TAG = "Wayne";
+    /**每一个线程需要下载的大小 */
+    private long blockSize;
+    /*** 线程数量<br> 默认为5个线程下载*/
+    private int threadNum = 5;
+    /*** 文件大小 */
+    private long fileSize;
+
     private int numProgress = 0;
 
     private ProgressBar progressBar;
 
-    private static ExecutorService threadPoolExecutor = new ThreadPoolExecutor(
-            3,
-            3,
+    private static final ExecutorService threadPoolExecutor = new ThreadPoolExecutor(
+            DataManager.getThreadNum(),
+            DataManager.getThreadNum(),
             0L,
             TimeUnit.MILLISECONDS,
             new LinkedBlockingQueue<>(1024),
             new DownloadThreadFactory(),
             new ThreadPoolExecutor.AbortPolicy());
 
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(@NonNull @NotNull Message msg) {
-            switch (msg.what){
-                case UPDATE_TEXT:
-                    showResponse(responseData);
-                    break;
-                case DOWNLOAD_SUCCESS:
-                    showResponse("下载成功");
-                    break;
-                case DOWNLOAD_FAIL:
-                    showResponse("下载失败");
-                    break;
-                case DOWNLOAD_PROGRESS:
-                    StringBuilder sb = new StringBuilder();
-                    double numPro = (double) numProgress/100;
-                    sb.append(numPro).append("%");
-                    showResponse(sb.toString());
-                    progressBar.setProgress(numProgress);
-                default:
-                    break;
-            }
-        }
-    };
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,27 +80,42 @@ public class DownloadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_download);
         textView = findViewById(R.id.textView);
         progressBar = findViewById(R.id.progressBar);
-        threadPoolExecutor.execute(new Runnable() {
+        handler = new Handler(){
             @Override
-            public void run() {
-                download();
-//                try {
-//                    sendRequestWithOkhttp();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+            public void handleMessage(@NonNull @NotNull Message msg) {
+                switch (msg.what){
+                    case UPDATE_TEXT:
+                        showResponse(responseData);
+                        break;
+                    case DOWNLOAD_SUCCESS:
+                        showResponse("下载成功");
+                        break;
+                    case DOWNLOAD_FAIL:
+                        showResponse("下载失败");
+                        break;
+                    case DOWNLOAD_PROGRESS:
+                        StringBuilder sb = new StringBuilder();
+                        double numPro = (double) numProgress/100;
+                        sb.append(numPro).append("%");
+                        showResponse(sb.toString());
+                        progressBar.setProgress(numProgress);
+                    default:
+                        break;
+                }
             }
-        });
+        };
+        try {
+            download();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void download() {
-        String url = "https://downloads.gradle-dn.com/distributions/gradle-6.7.1-bin.zip";
-        String fileName = "TARGET_FILE";
-        String filesDirPath = getFilesDir().getPath();
+    private void download() throws IOException{
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder()
                 .get()
-                .url(url)
+                .url(DataManager.getURL())
                 .build();
         Call call = okHttpClient.newCall(request);
 
@@ -129,61 +128,39 @@ public class DownloadActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                // Log.d(TAG, "onResponse.");
-
-                InputStream inputStream = Objects.requireNonNull(response.body()).byteStream();
-                File target = new File(filesDirPath, fileName);
-                FileOutputStream fileOutputStream = new FileOutputStream(target);
-                long total = Objects.requireNonNull(response.body()).contentLength();
-//                Log.d(TAG, String.valueOf(response.body().contentLength()));
-
+                DataManager.setFileSize(Objects.requireNonNull(response.body()).contentLength());
+                long fileSize = DataManager.getFileSize();
+                //只创建一个文件，saveFile下载内容
+                File saveFile = new File(DataManager.getSavePath() + "/" + DataManager.getFileName());
+                Log.e(TAG, "文件一共：" + fileSize + " savePath " + DataManager.getSavePath() + "  fileName  " + DataManager.getFileName());
+                RandomAccessFile accessFile = new RandomAccessFile(saveFile, "rwd");
+                //设置本地文件的长度和下载文件相同
+                accessFile.setLength(DataManager.getFileSize());
+                accessFile.close();
+                //每块线程下载数据
+                blockSize = ((fileSize % threadNum) == 0) ? (fileSize / threadNum) : (fileSize / threadNum + 1);
+                Log.e(TAG, "每个线程分别下载 ：" + blockSize);
                 try {
-                    byte[] buffer = new byte[2048];
-                    int len;
-                    long sum = 0;
-                    while ((len = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, len);
-                        sum+=len;
-                        double temp = (double) sum/total;
-                        temp*=10000;
-//                        Log.d(TAG, String.valueOf((int)temp));
-                        numProgress = (int) temp;
-                        sendMessage(DOWNLOAD_PROGRESS);
-//                         Log.d(TAG, "read: " + len);
-                    }
-                    fileOutputStream.flush();
-                } catch (IOException e) {
+                    threadPoolExecutor.execute(new DownloadThread(handler,blockSize,0,saveFile));
+                    threadPoolExecutor.execute(new DownloadThread(handler,blockSize,1,saveFile));
+                    threadPoolExecutor.execute(new DownloadThread(handler,blockSize,2,saveFile));
+                    threadPoolExecutor.execute(new DownloadThread(handler,blockSize,3,saveFile));
+                    threadPoolExecutor.execute(new DownloadThread(handler,blockSize,4,saveFile));
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                sendMessage(DOWNLOAD_SUCCESS);
             }
         });
-
     }
-
-    private void sendRequestWithOkhttp() throws IOException {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://www.baidu.com/")
-                .build();
-        Response response = okHttpClient.newCall(request).execute();
-        if (response.body() != null){
-            responseData = response.body().string();
-            Log.d(TAG, responseData);
-        }
-        sendMessage(UPDATE_TEXT);
-    }
-
-    private void sendMessage(int flag){
+    private void sendMessage(int what){
         Message msg = new Message();
-        msg.what = flag;
+        msg.what = what;
         handler.sendMessage(msg);
     }
 
     private void showResponse(String responseData) {
         textView.setText(responseData);
     }
-
 
     @Override
     protected void onDestroy() {
--- app/src/main/java/com/wayne/myapplication/DownloadThread.java
+++ app/src/main/java/com/wayne/myapplication/DownloadThread.java
@@ -1,62 +1,155 @@
 package com.wayne.myapplication;
 
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.URL;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * @author sunbowen
  */
 public class DownloadThread extends Thread{
+
+    private static final int BUFF_SIZE = 2048;
+
+    /**缓存的FIle*/
+    private File file;
+    /**开始位置*/
+    private int startPosition;
+    /**结束位置*/
+    private long endPosition;
+    /**当前位置*/
+    private int curPosition;
+    /**完成*/
+    private boolean finished = false;
+    /**已经下载多少*/
+    private int downloadSize = 0;
+
     public int nowNumProgress;
 
+    private static final String TAG = "Wayne";
+    /**每一个线程需要下载的大小 */
+    private long blockSize;
+
+    /**线程编号*/
+    private int ThreadNo;
+    /**下载的百分比*/
+    private int downloadPercent = 0;
+    /**下载的 平均速度*/
+    private int downloadSpeed = 0;
+    /**下载用的时间*/
+    private int usedTime = 0;
+    /**当前时间*/
+    private long curTime;
+
+    private Handler handler ;
+
+    /**
+     * 下载的构造函数
+     * @param handler   UI更新
+     */
+    public DownloadThread(Handler handler, long blockSize, int ThreadNo, File targetFile) throws InterruptedException {
+        this.handler = handler;
+        this.blockSize = blockSize;
+        this.ThreadNo = ThreadNo;
+        this.file = targetFile;
+        Log.e(TAG, toString());
+    }
+
+    @Override
+    public void run() {
+        long curThreadEndPosition = (ThreadNo+1) != DataManager.getThreadNum() ? ((ThreadNo+1)*blockSize-1) : DataManager.getFileSize();
+        endPosition  = curThreadEndPosition;
+        byte[] buf = new byte[BUFF_SIZE];
+        startPosition = downloadSize+1;
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .addHeader("RANGE", "bytes=" + downloadSize + "-" + DataManager.getFileSize())
+                .url(DataManager.getURL())
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                //
+                sendMessage(DataManager.getDownloadFail());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                RandomAccessFile rAccessFile = new RandomAccessFile(file, "rwd");//读写
+                BufferedInputStream bis = new BufferedInputStream(Objects.requireNonNull(response.body()).byteStream(), BUFF_SIZE);
+                try {
+                    //设置从什么位置开始写入数据
+                    rAccessFile.seek(startPosition);
+                    curPosition = startPosition;
+                    while (curPosition < endPosition)  //当前位置小于结束位置  继续下载
+                    {
+                        int len = bis.read(buf, 0, BUFF_SIZE);
+                        if (len == -1)   //下载完成
+                        {
+                            break;
+                        }
+                        rAccessFile.write(buf, 0, len);
+                        curPosition = curPosition + len;
+                        if (curPosition > endPosition) {    //如果下载多了，则减去多余部分
+                            System.out.println("  curPosition > endPosition  !!!!");
+                            long extraLen = curPosition - endPosition;
+                            downloadSize += (len - extraLen + 1);
+                        } else {
+                            downloadSize += len;
+                        }
+                    }
+                    finished = true;  //当前阶段下载完成
+                    Log.e(TAG, "当前线程" + ThreadNo + "下载完成");
+                } catch (Exception e) {
+                    Log.e(TAG, "download error Exception " + e.getMessage());
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        //关闭流
+                        bis.close();
+                        rAccessFile.close();
+                    } catch (IOException e) {
+                        Log.e("AccessFile", "AccessFile IOException " + e.getMessage());
+                    }
+                }
+            }
+        });
+        sendMessage(DataManager.getDownloadSuccess());
+    }
+
+
+    /**
+     * 发送消息，用户提示
+     * */
+    private void sendMessage(int what)
+    {
+        Message msg = new Message();
+        msg.what = what;
+        handler.sendMessage(msg);
+    }
 
 
 //    private void download() {
-//        String url = "https://downloads.gradle-dn.com/distributions/gradle-6.7.1-bin.zip";
-//        String fileName = "TARGET_FILE";
-//        String filesDirPath = getFilesDir().getPath();
-//        OkHttpClient okHttpClient = new OkHttpClient();
-//        Request request = new Request.Builder()
-//                .get()
-//                .url(url)
-//                .build();
-//        Call call = okHttpClient.newCall(request);
-//
-//        call.enqueue(new Callback() {
-//            @Override
-//            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                //
-//                sendMessage(DOWNLOAD_FAIL);
-//            }
-//
-//            @Override
-//            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//                // Log.d(TAG, "onResponse.");
-//
-//                InputStream inputStream = Objects.requireNonNull(response.body()).byteStream();
-//                File target = new File(filesDirPath, fileName);
-//                FileOutputStream fileOutputStream = new FileOutputStream(target);
-//                long total = Objects.requireNonNull(response.body()).contentLength();
-////                Log.d(TAG, String.valueOf(response.body().contentLength()));
-//
-//                try {
-//                    byte[] buffer = new byte[2048];
-//                    int len;
-//                    long sum = 0;
-//                    while ((len = inputStream.read(buffer)) != -1) {
-//                        fileOutputStream.write(buffer, 0, len);
-//                        sum+=len;
-//                        double temp = (double) sum/total;
-//                        temp*=10000;
-////                        Log.d(TAG, String.valueOf((int)temp));
-//                        numProgress = (int) temp;
-//                        sendMessage(DOWNLOAD_PROGRESS);
-////                         Log.d(TAG, "read: " + len);
-//                    }
-//                    fileOutputStream.flush();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                sendMessage(DOWNLOAD_SUCCESS);
-//            }
-//        });
+
 
 //    }
 }
